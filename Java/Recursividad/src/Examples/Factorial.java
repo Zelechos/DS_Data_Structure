fcaec048aaa8fc2c8b63ceefb9c3b5f4edab6e30
Argus Aphocraphex <zelechos@hotmail.com>
2023-02-13 18:40:24 -0400

    v0.0.3

--- Java/Recursividad/src/Examples/Factorial.java
+++ Java/Recursividad/src/Examples/Factorial.java
@@ -1,22 +1,22 @@
 package Examples;
 
 /**
- *
- * @author Pragmatic Coder
+ * @author Alex T.H.
  */
 public class Factorial {
 
+    static int NUMBER_TEST = 9;
+
     public static void main(String[] args) {
-        System.out.println(Factorial(9));
+        System.out.println(factorial(NUMBER_TEST));
     }
-    
-    
-// Subrutina para hallar el factorial de un numero
-    public static int Factorial(int Number) {
-        if (Number == 0 || Number == 1) {
+
+    // Subrutina para hallar el factorial de un numero
+    public static int factorial(int number) {
+        if (number == 0 || number == 1) {
             return 1;
         }
-        return Number * Factorial(Number - 1);
+        return number * factorial(number - 1);
     }
-    
+
 }
